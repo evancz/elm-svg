1fd3f227da6f65ae889717a4008efdebe00adf1b
Evan Czaplicki <info@elm-lang.org>
2014-12-18 23:51:08 -0600

    Add all of the relevant event handlers

---
+++ src/Svg/Events.elm
@@ -0,0 +1,35 @@
+module Svg.Events where
+{-|
+
+-}
+
+
+-- Animation event attributes
+
+onBegin
+onEnd
+onLoad
+onRepeat
+
+-- Document event attributes
+
+onAbort
+onError
+onResize
+onScroll
+onUnload
+onZoom
+
+-- Graphical event attributes
+
+onActivate
+onClick
+onFocusIn
+onFocusOut
+onLoad
+onMouseDown
+onMouseMove
+onMouseOut
+onMouseOver
+onMouseUp
+
